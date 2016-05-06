aad47f106918292e78f8c69fc82c67057535872c
Jessica Oakes <jessica.yoonah@gmail.com>
2016-05-06 11:45:40 -0500

    Adds login posting

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 var router = require("express").Router();
 var path = require("path");
+var passport = require("passport");
+
 var wishlist = require("./wishlist");
 var login = require("./login");
 var register = require("./register");
@@ -11,6 +13,13 @@ router.use("/wishlist", wishlist);
 router.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/views/index.html"));
 });
+
+router.post("/", function(req, res) {
+  console.log('attempting to authenticate');
+  passport.authenticate("local")(req, res, function() {
+    res.sendStatus(200);
+  });
+}); //  router.post("/")
 
 router.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/views/index.html"));
