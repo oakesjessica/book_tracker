5ae5c1c3f8ac98e526c43c4e3e525daf47c94bee
Jessica Oakes <jessica.yoonah@gmail.com>
2016-09-09 22:33:03 -0500

    Fixes max call stack error

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,7 +3,7 @@ var router = require("express").Router();
 var path = require("path");
 
 router.get("/", function(req, res, next) {
-  res.sendFile(path.resolve(__dirname, "../public/views/login.html"));
+  res.sendFile(path.resolve(__dirname, "../public/views/partials/login.html"));
 }); //  router.get("/")
 
 module.exports = router;
